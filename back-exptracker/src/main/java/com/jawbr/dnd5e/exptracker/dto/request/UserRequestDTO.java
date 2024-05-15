b0ad730456602b67b550c63cb35f5df91eceab43
Bradley Jawwad <bradleyjawwad@hotmail.com>
2024-05-15 19:58:22 -0300

    Add Register new User minimum size username/password

--- back-exptracker/src/main/java/com/jawbr/dnd5e/exptracker/dto/request/UserRequestDTO.java
+++ back-exptracker/src/main/java/com/jawbr/dnd5e/exptracker/dto/request/UserRequestDTO.java
@@ -2,13 +2,18 @@ package com.jawbr.dnd5e.exptracker.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record UserRequestDTO(
-        @NotBlank(message = "Name cannot be empty!") String username,
+        @NotBlank(message = "Name cannot be empty!")
+        @Size(min = 3, message = "Name must have at least 3 characters!")
+        String username,
         @NotBlank(message = "Email cannot be empty!")
         @Email(message = "Invalid email format!") String email,
-        @NotBlank(message = "Password cannot be empty!") String password
+        @NotBlank(message = "Password cannot be empty!")
+        @Size(min = 6, message = "Password must have at least 6 characters!")
+        String password
 ) {
 }
